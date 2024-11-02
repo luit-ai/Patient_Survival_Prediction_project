51f103e78445dc5a48bac5f47e2c2b3650f72ae9
luit-ai <msarma.ngn@gmail.com>
2024-11-02 16:43:57 +0530

    updated

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,10 +18,10 @@ def sample_input_data():
     # divide train and test
     X_train, X_test, y_train, y_test = train_test_split(
         
-        data[config.model_config.features],     # predictors
-        data[config.model_config.target],       # target
-        test_size = config.model_config.test_size,
-        random_state=config.model_config.random_state,   # set the random seed here for reproducibility
+        data[config.self_model_config.features],     # predictors
+        data[config.self_model_config.target],       # target
+        test_size = config.self_model_config.test_size,
+        random_state=config.self_model_config.random_state,   # set the random seed here for reproducibility
     )
 
     return X_test, y_test
